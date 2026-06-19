fbf74d26fce42ee1b5cb676a5f9c4c00156839d4
minlin125 <simplemin125@gmail.com>
2026-06-19 12:35:35 +0800

    CODEX AI輔助滑動功能的撰寫

--- button.js
+++ button.js
@@ -1,8 +1,9 @@
 
-  const blogList = document.querySelector('.blog-list');
-  const prevButton = document.querySelector('.blog-slider-button-prev');
-  const nextButton = document.querySelector('.blog-slider-button-next');
+const blogList = document.querySelector('.blog-list');
+const prevButton = document.querySelector('.blog-slider-button-prev');
+const nextButton = document.querySelector('.blog-slider-button-next');
 
+if (blogList && prevButton && nextButton) {
   prevButton.addEventListener('click', function () {
     blogList.scrollBy({
       left: -384,
@@ -16,3 +17,37 @@
       behavior: 'smooth'
     });
   });
+}
+
+if (blogList) {
+  let isDragging = false;
+  let startX = 0;
+  let scrollStart = 0;
+
+  blogList.addEventListener('pointerdown', function (event) {
+    isDragging = true;
+    startX = event.clientX;
+    scrollStart = blogList.scrollLeft;
+    blogList.classList.add('is-dragging');
+    blogList.setPointerCapture(event.pointerId);
+  });
+
+  blogList.addEventListener('pointermove', function (event) {
+    if (!isDragging) {
+      return;
+    }
+
+    const moveX = event.clientX - startX;
+    blogList.scrollLeft = scrollStart - moveX;
+  });
+
+  blogList.addEventListener('pointerup', function () {
+    isDragging = false;
+    blogList.classList.remove('is-dragging');
+  });
+
+  blogList.addEventListener('pointercancel', function () {
+    isDragging = false;
+    blogList.classList.remove('is-dragging');
+  });
+}
